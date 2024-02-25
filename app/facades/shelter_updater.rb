62c32fb868d6e3e2edba6a6b96700f54a4f5fbcf
Nash <141100397+s2an@users.noreply.github.com>
2024-02-24 23:18:17 -0500

    chore: Delete old code

--- app/facades/shelter_updater.rb
+++ app/facades/shelter_updater.rb
@@ -6,15 +6,6 @@ class ShelterUpdater
         end
 
         ShelterSerializer.new(Shelter.update!(shelter_id, shelter_params))
-        
-        # shelter = Shelter.find_by(id: shelter_id)
 
-        # if shelter == nil #fail
-        #     return { json: { error: "Shelter not found" }, status: :not_found }
-        # elsif shelter.update(shelter_params) #success
-        #     return { json: ShelterSerializer.new(shelter), status: :ok }
-        # else #fail
-        #     return { json: { message: "Shelter update failed" }, status: :unprocessable_entity }
-        # end
     end
 end
