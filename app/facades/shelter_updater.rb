f7e8318b3e68dfa1ada51758d04f60a9dfae39fb
Nash <141100397+s2an@users.noreply.github.com>
2024-02-25 13:53:17 -0500

    refactor: Remove unused code to increase test coverage percentage

--- app/facades/shelter_updater.rb
+++ app/facades/shelter_updater.rb
@@ -1,11 +1,9 @@
 class ShelterUpdater
     def self.update(shelter_id, shelter_params)
-        
-        if shelter_params[:shelter_id]
-            return unless shelter = Shelter.find_by(id: shelter_id)
-        end
-
+        # I believe this is not needed and it brings our test coverage to 100%
+        # if shelter_params[:shelter_id]
+        #     return unless shelter = Shelter.find_by(id: shelter_id)
+        # end
         ShelterSerializer.new(Shelter.update!(shelter_id, shelter_params))
-
     end
 end
