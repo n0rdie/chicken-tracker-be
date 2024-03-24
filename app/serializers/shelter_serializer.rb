28cdaee438f6604a217e1efcb3342c9d699b004c
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-03-24 11:34:02 -0600

    Add animals to shelter serializer

--- app/serializers/shelter_serializer.rb
+++ app/serializers/shelter_serializer.rb
@@ -1,7 +1,7 @@
 class ShelterSerializer
     include JSONAPI::Serializer
 
-    attributes :name, :user_id
+    attributes :name, :user_id, :animals
 
     has_many :animals, serializer: AnimalSerializer
 end
